a925caf3c06d5685b42553304cda05dc4ef90652
Xiaozi <73756250+hxxiaozi@users.noreply.github.com>
2024-02-28 10:42:48 -0600

    *
    gradient

--- 2.js
+++ 2.js
@@ -1,19 +1,28 @@
 document.addEventListener("DOMContentLoaded", function () {
     const videoPlayer = document.getElementById("video-player");
-    const videoList = document.querySelectorAll(".video-item a");
+    const videoList = document.querySelectorAll(".video-item");
 
-    videoList.forEach((videoLink) => {
-        videoLink.addEventListener("click", function (e) {
+    videoList.forEach((videoItem) => {
+        videoItem.addEventListener("click", function (e) {
             e.preventDefault();
-            const videoSource = this.getAttribute("data-video");
+            const videoSource = this.querySelector("a").getAttribute("data-video");
             videoPlayer.src = videoSource;
             videoPlayer.load();
             videoPlayer.play();
+
+            // Remove the 'selected' class from all video items
+            videoList.forEach((item) => {
+                item.classList.remove("selected");
+            });
+
+            // Add the 'selected' class to the clicked video item's parent div
+            this.classList.add("selected");
         });
     });
 });
+
 document.addEventListener("mousemove", (e) => {
     const cursor = document.getElementById("custom-cursor2");
     cursor.style.left = e.clientX + "px";
     cursor.style.top = e.clientY + "px";
-  });
+});
