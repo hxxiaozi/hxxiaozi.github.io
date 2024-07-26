6a0e07dd628cc1bddfd4a0cceebc6468b2e04da6
Xiaozi <73756250+hxxiaozi@users.noreply.github.com>
2024-07-26 21:41:37 +0800

    fix switch video func

--- index.js
+++ index.js
@@ -1,5 +1,28 @@
 document.addEventListener("mousemove", (e) => {
-    const cursor = document.getElementById("custom-cursor0");
-    cursor.style.left = e.clientX + "px";
-    cursor.style.top = e.clientY + "px";
+  const cursor = document.getElementById("custom-cursor0");
+  cursor.style.left = e.clientX + "px";
+  cursor.style.top = e.clientY + "px";
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const videoPlayer = document.querySelector(".video-player");
+  const videoList = document.querySelectorAll(".video-item");
+
+  videoList.forEach((videoItem) => {
+    videoItem.addEventListener("click", function (e) {
+      e.preventDefault();
+      const videoSource = this.querySelector("a").getAttribute("data-video");
+      videoPlayer.src = videoSource;
+      videoPlayer.load();
+      videoPlayer.play();
+
+      // Remove the 'selected' class from all video items
+      videoList.forEach((item) => {
+        item.classList.remove("selected");
+      });
+
+      // Add the 'selected' class to the clicked video item's parent div
+      this.classList.add("selected");
+    });
   });
+});
