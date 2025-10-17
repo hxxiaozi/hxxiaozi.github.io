a186c9c54f69f460bbb0a3ff35629a3c516a2019
Xiaozi <73756250+hxxiaozi@users.noreply.github.com>
2025-10-17 12:06:16 -0400

    navbar update

--- 3.js
+++ 3.js
@@ -1,9 +1,5 @@
-
-
-
-
-  document.addEventListener("mousemove", (e) => {
-    const cursor = document.getElementById("custom-cursor3");
-    cursor.style.left = e.clientX + "px";
-    cursor.style.top = e.clientY + "px";
-  });
+document.addEventListener("mousemove", (e) => {
+  const cursor = document.getElementById("custom-cursor3");
+  cursor.style.left = e.clientX + "px";
+  cursor.style.top = e.clientY + "px";
+});
